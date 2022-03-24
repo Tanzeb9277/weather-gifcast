f39b376628be5949ed69aea755e099f7893c0bed
Tanzeb Choudhury <Tanzeem123456@gmail.com>
2022-03-24 17:15:24 -0400

    Displays gif based on the weather

---
+++ script.js
@@ -0,0 +1,56 @@
+const img = document.querySelector('img');
+const temp = document.querySelector('#temp');
+const input = document.querySelector('#location');
+const search = document.querySelector('#search');
+const scale =  document.querySelector('#scale')
+const convert = document.querySelector('#convert');
+const convertScale = document.querySelector('#convert-scale');
+let currentMeasurement = 'imperial';
+let currentLocation = 'Atlanta';
+
+async function getGif(gif) {
+  const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=mkR6Xw4tvFCKgBhYJuPQHhZj5Mafgrot&s=${gif}`, {mode: 'cors'});
+  const catData = await response.json();
+  img.src = catData.data.images.original.url;
+}
+
+async function getWeather(location='Atlanta', measurement='imperial') {
+    currentMeasurement = measurement;
+    currentLocation = location;
+    try{
+        const response = await 
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=772bfda95d2ceb52b45e3502623a9f03&units=${measurement}`
+        , {mode: 'cors'});
+        const weatherData = await response.json();
+        temp.innerText = Math.round(weatherData.main.temp);
+        if(measurement == 'imperial'){
+            scale.innerText = 'F'
+        }else{
+            scale.innerText = 'C'
+        }
+        getGif(weatherData.weather[0].description)
+    }catch(err) {
+        alert("Location not found"); // TypeError: failed to fetch
+    }
+
+    
+  }
+
+search.addEventListener('click', function(){
+    let location = input.value;
+    getWeather(location)
+})
+convert.addEventListener('click', function(){
+    let location = currentLocation;
+    if(currentMeasurement == 'imperial' ){
+        getWeather(location, 'metric');
+        convertScale.innerText = 'F'
+    }else{
+        
+        getWeather(location, 'imperial');
+        convertScale.innerText = 'C'
+    }
+    
+})
+
+getWeather();
